693c38e240818af9aca5065624fbc4cbb509ea16
kalz2q <katsu95i@gmail.com>
2018-03-30 16:27:44 +0900

    m

--- mycpp/hello02.cpp
+++ mycpp/hello02.cpp
@@ -1,6 +1,7 @@
 //hello02.cpp
 #include <iostream>
-using namespace std;
-int main() {
-    cout << "hello world\n \t初めてのC++" << endl;
+template <typename ...T>
+int hello() {
+    std::cout << "hello world\n \t初めてのC++" << std::endl;
+    return 0;
 }
